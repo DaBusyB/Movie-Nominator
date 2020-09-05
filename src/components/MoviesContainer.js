ab487670f1693cad655c56110386e0e6210a720c
DaBusyB <brandiapetsi@gmail.com>
2020-09-05 18:14:48 -0400

    Add MoviesContainer starter

---
+++ src/components/MoviesContainer.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+// Import Components
+import MovieCard from './MovieCard'
+
+
+// Component
+export default function MovieContainer() {
+
+
+    return (
+        <div className='container'>
+
+        </div>
+    )
+}
