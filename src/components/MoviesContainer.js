60afb34e6f0741e8fbe81c6c8c6f89ca7fb9a27a
DaBusyB <brandiapetsi@gmail.com>
2020-09-06 14:36:05 -0400

    Normalize search results

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -13,22 +13,48 @@ export default function MovieContainer() {
     const [movies, setMovies] = useState([])
     const [titleSearch, setTitleSearch] = useState(`star+wars`)
 
+    //normalize all search events by replacing spaces with '+'
+    const normalizeSearchData = event => {
+        return event.replace(/\s/g, '+')
+    }
+
+    // search for our normalized event
     const searchHandler = event => {
-        setTitleSearch(event.target.value)
+        console.log('search handler:', event.target.value)
+        setTitleSearch(normalizeSearchData(event.target.value))
     }
 
     useEffect(() => {
-
         axios
         .get(`http://omdbapi.com/?s=${titleSearch}&apikey=82e86859`)
             .then((res) => {
-                // console.log('axios success:', res.data.Search)
-                setMovies(res.data.Search)
+                console.log('axios success:', res.data.Search)
+                
+                res.data.Search === undefined ?
+                    // res.data.Search will be undefined at times while searching
+                    // here we provide something for users to see until search returns something
+                    setTitleSearch(`star+wars`)
+                :
+                    setMovies(res.data.Search)
             })
             .catch((err) => {
+                // console.log('axios err:', err)
                 // setMovies(`${titleSearch} cannot be found, try again`)
             })
     }, [titleSearch])
+
+    // useEffect(() => {
+
+    //     axios
+    //     .get(`http://omdbapi.com/?s=${black panther}&apikey=82e86859`)
+    //         .then((res) => {
+    //             // console.log('axios success:', res.data.Search)
+                
+    //         })
+    //         .catch((err) => {
+    //             // setMovies(`${titleSearch} cannot be found, try again`)
+    //         })
+    // }, [])
 
     return (
         <div className='search_movie_container'>
@@ -39,9 +65,7 @@ export default function MovieContainer() {
             <Container className='container'>
                 <Row>
                     {
-                        movies === undefined ?
-                            console.log('movies is undefined')
-                        :
+                        
                             movies.map( movie => {
                                 return <MovieCard key={movie.imdbID} movieInfo={movie} />
                             })
