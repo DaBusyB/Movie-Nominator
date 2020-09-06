e358a0b1cd7383ff7eca209e8c7f3813481973ed
DaBusyB <brandiapetsi@gmail.com>
2020-09-05 20:13:26 -0400

    Add MovieCard component to MoviesContainer

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -12,18 +12,18 @@ export default function MovieContainer() {
         axios
             .get(`http://omdbapi.com/?s=star+wars&apikey=82e86859`)
             .then((res) => {
-                console.log('axios success:', res.data.Search)
+                // console.log('axios success:', res.data.Search)
                 setMovies(res.data.Search)
             })
             .catch((err) => {
                 console.log(err)
             })
-    }, [movies])
+    }, [])
 
     return (
         <div>
             {
-                {/* movies.map(movie => (console.log('movie here:', movie))) */}
+                movies.map( movie => ( <MovieCard key={movie.imdbID} movie={movie} /> ) )
             }
         </div>
     )
